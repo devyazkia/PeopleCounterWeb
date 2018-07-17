74cd6a3bfa323e27d3589f39d794532d0ea556ac
devyazkia <devyazkiaputri@gmail.com>
2018-07-17 07:36:29 +0700

    export (current)

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -4,17 +4,19 @@ from django.urls import path
 from rest_framework import routers
 from . import views
 # from myproject.views import ,get_data
-from .views import ChartData, getHistory, getData, GeneratePDF
+from .views import ChartData, getHistory, getData
 
 urlpatterns = [
     path('api/chart/data/', ChartData.as_view()),
     path('api/chart/datahistory/', getHistory.as_view()),
     path('', views.index, name='index'),
-    # path('pdf/', GeneratePDF.as_view()),
-    path('history/', views.history, name='history'),    
-    # path(r'^generate_pdf/$', "analytics.views.GroupPDFGenerate.as_view()", name="generate_pdf"),
+    path('history/', views.history, name='history'),
+    path('export/', views.export, name='export'),   
+    path('^export/csv/$', views.export_users_csv, name='export_users_csv'),  
     path('admin/', admin.site.urls),
     path('history/date=<date>', views.getData),
+    path('exporthistory/csv/$', views.export_history, name='export_history'),  
+    path('exportrange/csv/$', views.export_range, name='export_range'),      
     # path('history/date=<date>', views.getData),
     # path('^posts/(?P<slug>[-\w]+)/$', views.getHistory),
     # path(r'^$', views.index, name="index"), 
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,8 +1,10 @@
+import csv
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View, ListView
 from django.template.loader import get_template
+from django.contrib.auth.models import User
 
 from .forms import DataForm
 from .model import rekap_pengunjung
@@ -33,6 +35,11 @@ def history(request):
     form = DataForm()
     return render(request, 'history.html', {'form': form, 'datas': rekap_pengunjung.objects.filter(tanggal= get), 'status': status })  
 
+def export(request):
+    status = ''
+    form = DataForm()
+    return render(request, 'export.html', {'form': form, 'datas': rekap_pengunjung.objects.all(), 'status': status })  
+    
 class getHistory(APIView):
     
     authentication_classes = []
@@ -73,6 +80,36 @@ def getData(request, date):
     form = DataForm()
     return render(request, 'history.html', {'form': form, 'datas': rekap_pengunjung.objects.filter(tanggal= '{}'.format(date)), 'status': status })
 
+def export_history(request, date):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="result.csv"'
+        
+    writer = csv.writer(response)
+    writer.writerow(['Tanggal', 'pukul 10', 'pukul 11', 'pukul 12', 'pukul 13', 'pukul 14', 'pukul 15', 'pukul 16', 'pukul 17', 'pukul 18', 'pukul 19', 'pukul 20', 'pukul 21'])
+
+    users = rekap_pengunjung.objects.filter(tanggal= '{}'.format(date)).values_list('tanggal', 'pukul10', 'pukul11', 'pukul12', 'pukul13', 'pukul14', 'pukul15', 'pukul16', 'pukul17', 'pukul18', 'pukul19', 'pukul20', 'pukul21')
+    for user in users:
+        writer.writerow(user)
+
+    return response
+
+def export_range(request):
+    first_date = datetime.date(2018, 6, 29)
+    last_date = datetime.date(2018, 7, 12)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="result.csv"'
+        
+    writer = csv.writer(response)
+    writer.writerow(['Tanggal', 'pukul 10', 'pukul 11', 'pukul 12', 'pukul 13', 'pukul 14', 'pukul 15', 'pukul 16', 'pukul 17', 'pukul 18', 'pukul 19', 'pukul 20', 'pukul 21'])
+
+    users = rekap_pengunjung.objects.filter(tanggal=(first_date, last_date)).values_list('tanggal', 'pukul10', 'pukul11', 'pukul12', 'pukul13', 'pukul14', 'pukul15', 'pukul16', 'pukul17', 'pukul18', 'pukul19', 'pukul20', 'pukul21')
+    for user in users:
+        writer.writerow(user)
+
+    return response
+
+
 class ChartData(APIView):
 
     authentication_classes = []
@@ -85,7 +122,6 @@ class ChartData(APIView):
         data = []
         rowarray_list = []
         for row in rows:
-            # t9 = json.dumps((row.pukul09))            
             t10 = json.dumps((row.pukul10))
             t11 = json.dumps((row.pukul11))
             t12 = json.dumps((row.pukul12))
@@ -109,56 +145,31 @@ class ChartData(APIView):
         }
         return Response(datajson)
 
+
+def export_users_csv(request):
+    now = datetime.datetime.now()
+    get = now.strftime("%Y-%m-%d")
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="result.csv"'
+    
+    writer = csv.writer(response)
+    writer.writerow(['Tanggal', 'pukul 10', 'pukul 11', 'pukul 12', 'pukul 13', 'pukul 14', 'pukul 15', 'pukul 16', 'pukul 17', 'pukul 18', 'pukul 19', 'pukul 20', 'pukul 21'])
+
+    users = rekap_pengunjung.objects.filter(tanggal= '{}'.format(get)).values_list('tanggal', 'pukul10', 'pukul11', 'pukul12', 'pukul13', 'pukul14', 'pukul15', 'pukul16', 'pukul17', 'pukul18', 'pukul19', 'pukul20', 'pukul21')
+    for user in users:
+        writer.writerow(user)
+
+    return response
+
+
 # class GeneratePDF(View):
 #     def get(self, request, *args, **kwargs):
-#         template =  get_template('index.html')
-#         rows = rekap_pengunjung.objects.filter(tanggal= get)
-#         for row in rows:
-#             # t9 = json.dumps((row.pukul09))            
-#             t10 = json.dumps((row.pukul10))
-#             t11 = json.dumps((row.pukul11))
-#             t12 = json.dumps((row.pukul12))
-#             t13 = json.dumps((row.pukul13))
-#             t14 = json.dumps((row.pukul14))
-#             t15 = json.dumps((row.pukul15))
-#             t16 = json.dumps((row.pukul16))
-#             t17 = json.dumps((row.pukul17))
-#             t18 = json.dumps((row.pukul18))
-#             t19 = json.dumps((row.pukul19))
-#             t20 = json.dumps((row.pukul20))
-#             t21 = json.dumps((row.pukul21))
-#         context = {
-#             "rekap_pengunjung.tanggal" : get,
-#             "rekap_pengunjung.pukul10" : t10,
-#             "rekap_pengunjung.pukul11" : t11,
-#             "rekap_pengunjung.pukul12" : t12,
-#             "rekap_pengunjung.pukul13" : t13,
-#             "rekap_pengunjung.pukul14" : t14,
-#             "rekap_pengunjung.pukul15" : t15,
-#             "rekap_pengunjung.pukul16" : t16,
-#             "rekap_pengunjung.pukul17" : t17,
-#             "rekap_pengunjung.pukul18" : t18,
-#             "rekap_pengunjung.pukul19" : t19,
-#             "rekap_pengunjung.pukul20" : t20,
-#             "rekap_pengunjung.pukul21" : t21,
+#         data = {
+#             'today': datetime.date.today(), 
+#             'amount': 39.99,
+#             'customer_name': 'Cooper Mann',
+#             'order_id': 1233434,
 #         }
-#         html = template.render(context)
-#         pdf = render_to_pdf('index.html', context)
-#         return HttpResponse(pdf)
-
-#  from django.http import HttpResponse
-#  from django.views.generic import View
-
-#  from yourproject.utils import render_to_pdf #created in step 4
-
-class GeneratePDF(View):
-    def get(self, request, *args, **kwargs):
-        data = {
-            'today': datetime.date.today(), 
-            'amount': 39.99,
-            'customer_name': 'Cooper Mann',
-            'order_id': 1233434,
-        }
-        pdf = render_to_pdf('templates/coba.html', data)
-        return HttpResponse(pdf, content_type='myproject/template')
+#         pdf = render_to_pdf('templates/coba.html', data)
+#         return HttpResponse(pdf, content_type='myproject/template')
 
